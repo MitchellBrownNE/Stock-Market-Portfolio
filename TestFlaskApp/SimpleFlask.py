63687d09e809449e5114f5da53479e02883e7364
Mitchell <mitchellbrownne@gmail.com>
2024-09-07 17:23:51 -0400

    edit

--- TestFlaskApp/SimpleFlask.py
+++ TestFlaskApp/SimpleFlask.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 application = app
 
 @app.route('/')
 def index():
-    return "<h1>Hello world!</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
