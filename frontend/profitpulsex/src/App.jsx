c0504a9ca4432c0a190f990462d120e6097e5219
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-12 23:42:06 -0400

    Installed and set up react router so set up page routing
    Set starting path to the starting page by importing and calling its component with route path = "/" which signals the initial page when the app starts.

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,20 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StarterPage from "./pages/startingPage";
+
 function App() {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center h-screen bg-bgdark">
-        <h1 className="text-white text-6xl font-heading mb-8">
-          Profit Pulse X
-        </h1>
-        <div className="flex space-x-4">
-          <button className="bg-lightgreen font-body text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
-            Login
-          </button>
-          <button className="bg-boldred font-body text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none">
-            Register
-          </button>
-        </div>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<StarterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
