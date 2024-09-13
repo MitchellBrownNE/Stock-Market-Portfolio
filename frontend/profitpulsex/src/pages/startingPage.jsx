fe436dc357076ea7c4163db19fc2b909e3a98cbd
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-13 18:35:50 -0400

    add route to register page from starting page
    import useNavigate and call it when a user clicks on the register button to route them to the register page that was defined in app.jsx

--- frontend/profitpulsex/src/pages/startingPage.jsx
+++ frontend/profitpulsex/src/pages/startingPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function StarterPage() {
+  const navigate = useNavigate();
+
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen bg-bgdark">
@@ -11,7 +14,10 @@ function StarterPage() {
           <button className="bg-lightgreen font-body text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
             Login
           </button>
-          <button className="bg-boldred font-body text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none">
+          <button
+            className="bg-boldred font-body text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none"
+            onClick={() => navigate("/register")}
+          >
             Register
           </button>
         </div>
