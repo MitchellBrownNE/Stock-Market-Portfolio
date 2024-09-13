871157eef3b82116bc87599d08cd8bfa4cc5812d
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-13 18:25:10 -0400

    assign register page as a route path
    import register page component and assign register to a route path.

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StarterPage from "./pages/startingPage";
+import RegisterPage from "./pages/register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<StarterPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
