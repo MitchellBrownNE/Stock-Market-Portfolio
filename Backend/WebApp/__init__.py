87e64d8486001415b180872ecbf66e3db4f254b4
Mitchell <mitchellbrownne@gmail.com>
2024-09-13 18:14:50 -0400

    Added dockerfile and changed fronend folder

--- Backend/WebApp/__init__.py
+++ Backend/WebApp/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='../Frontend/stockmarket/build', static_url_path='')
+app = Flask(__name__, static_folder='../Frontend/build', static_url_path='')
 
 from WebApp import routes
 
