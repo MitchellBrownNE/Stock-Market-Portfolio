5da0514a422dc97b24d9b713589e313515fba384
Mitchell <mitchellbrownne@gmail.com>
2024-09-13 18:21:11 -0400

    Added dockerfile and changed fronend folder

--- Backend/WebApp/routes.py
+++ Backend/WebApp/routes.py
@@ -1,11 +1,12 @@
 from WebApp import app
-from flask import render_template
-from PredictionModel import stock_api as API
+from flask import send_from_directory
+import os
 
 #Route any of the webpages here, example page set in templates
-@app.route('/')
-def index():
-
-    data = API.DownloadData()
-
-    return render_template('index.html', data=data)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
