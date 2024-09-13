dc637636364a8291757c25c0592f827f7005d0b7
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-12 21:59:59 -0400

    remove import and add title of app
    Removed unnecessary import and added title of project to the initial screen when you start the app

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      return (
+      <div className="flex justify-center items-center h-screen bg-black">
+        <h1 className="text-white text-6xl">Profit Pulse X</h1>
+      </div>
+      );
+    </>
+  );
 }
 
 export default App;
