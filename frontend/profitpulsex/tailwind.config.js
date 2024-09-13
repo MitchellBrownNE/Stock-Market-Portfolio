903829ad378fb9ab62a7cab7e956fdcbf50214cf
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-12 22:30:47 -0400

    configured google fonts to tailwind
    added roboto and lora fonts to tailwind for easier styling

--- frontend/profitpulsex/tailwind.config.js
+++ frontend/profitpulsex/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // Lora will be used for headings -- using tailwind example "text-xl font-heading"
+        heading: ["Lora", "serif"],
+        // Roboto will be used for body text -- using tailwind example "text-sm font-body"
+        body: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
