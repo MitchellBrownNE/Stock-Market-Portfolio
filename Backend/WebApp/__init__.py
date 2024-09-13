f0c9ded036970b35f3ef3c045824f773c70fb483
Mitchell <mitchellbrownne@gmail.com>
2024-09-13 18:47:54 -0400

    Added dockerfile and changed fronend folder

--- Backend/WebApp/__init__.py
+++ Backend/WebApp/__init__.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='../Frontend/build', static_url_path='')
+# Get the absolute path to the React build folder
+app = Flask(__name__, static_folder=os.path.join(os.getcwd(), 'Frontend', 'build'), static_url_path='')
+
+print(f"Static folder: {app.static_folder}")  # For debugging
 
 from WebApp import routes
 
