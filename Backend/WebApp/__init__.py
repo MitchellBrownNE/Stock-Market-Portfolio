d2061220d04c34a62340c605f706933227cf0cbb
Mitchell <mitchellbrownne@gmail.com>
2024-09-13 17:09:47 -0400

    added react to flask

--- Backend/WebApp/__init__.py
+++ Backend/WebApp/__init__.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../Frontend/stockmarket/build', static_url_path='')
 
 from WebApp import routes
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.errorhandler(404)
+def not_found(e):
+    return send_from_directory(app.static_folder, 'index.html')
