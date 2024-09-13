18df5ef8bbac4c82b8566164412dfbc71f8ab7fc
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-12 23:00:43 -0400

    adjust start page to take new styles
    Added new configured tailwind styles that match color and font scheme to app

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,18 +1,19 @@
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen bg-black">
-        <h1 className="text-white text-6xl mb-8">Profit Pulse X</h1>
+      <div className="flex flex-col justify-center items-center h-screen bg-bgdark">
+        <h1 className="text-white text-6xl font-heading mb-8">
+          Profit Pulse X
+        </h1>
         <div className="flex space-x-4">
-          <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
+          <button className="bg-lightgreen font-body text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
             Login
           </button>
-          <button className="bg-green-500 text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none">
+          <button className="bg-boldred font-body text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none">
             Register
           </button>
         </div>
       </div>
-      );
     </>
   );
 }
