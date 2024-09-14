79ccaa7577948ca78295fff5696040c3bd5683a0
Mitchell <mitchellbrownne@gmail.com>
2024-09-14 15:33:59 -0400

    Added react files

---
+++ frontend/profitpulsex/src/pages/register.jsx
@@ -0,0 +1,11 @@
+function Register() {
+  return (
+    <>
+      <div className="text-xl font-heading flex flex-col justify-center items-center mt-10">
+        <div className="text-bgdark">Register Page</div>
+      </div>
+    </>
+  );
+}
+
+export default Register;
