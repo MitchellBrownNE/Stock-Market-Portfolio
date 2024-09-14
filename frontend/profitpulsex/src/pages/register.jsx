eff2e22550fa9d1822aa1449486e7dfa8411c531
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-14 13:56:59 -0400

    update register to remove unnecessay imporrt

--- frontend/profitpulsex/src/pages/register.jsx
+++ frontend/profitpulsex/src/pages/register.jsx
@@ -1,7 +1,11 @@
-import React from React
-
 function Register() {
-    return(
-        <></>
-    )
+  return (
+    <>
+      <div className="text-xl font-heading flex flex-col justify-center items-center mt-10">
+        <div className="text-bgdark">Register Page</div>
+      </div>
+    </>
+  );
 }
+
+export default Register;
