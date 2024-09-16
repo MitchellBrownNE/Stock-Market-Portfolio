d5a451b6d96862a58913be67a2e6c627c2c2811f
Mitchell Brown <MitchellBrownNE@gmail.com>
2024-09-16 16:45:17 -0400

    Added Kevins frontend files

---
+++ frontend/profitpulsex/src/pages/startingPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function StarterPage() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center h-screen bg-bgdark">
+        <h1 className="text-white text-6xl font-heading mb-8">Profit</h1>
+        <div className="flex space-x-4">
+          <button className="bg-lightgreen font-body text-white px-6 py-2 rounded-lg hover:bg-blue-600 focus:outline-none">
+            Login
+          </button>
+          <button
+            className="bg-boldred font-body text-white px-6 py-2 rounded-lg hover:bg-green-600 focus:outline-none"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default StarterPage;
--- frontend/profitpulsex/vite.config.js
+++ frontend/profitpulsex/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' // Ensure this is set to 'dist'
+    outDir: 'dist'
   }
 })
