8a52ea78a0a81c97c3f0c05bbbd64e0328091eee
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-09-19 23:10:35 -0400

    update app.jsx to accept login route
    import login page and add route

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import StarterPage from "./pages/startingPage";
 import RegisterPage from "./pages/register";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StarterPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
