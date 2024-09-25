191096f073ec57b4b9b33fc6c3c1571bc1b83e3f
Tareksilmi20 <tarek.silmi24@gmail.com>
2024-09-25 15:40:36 -0400

    updated navbar
    navbar will only be visable when logged in

--- frontend/profitpulsex/src/App.jsx
+++ frontend/profitpulsex/src/App.jsx
@@ -1,23 +1,39 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import StarterPage from "./pages/startingPage";
 import RegisterPage from "./pages/register";
 import LoginPage from "./pages/login";
 import Dashboard from "./pages/dashboard";
 import Navbar from './component/Navbar';
 
+function AppContent() {
+  const location = useLocation();
 
-function App() {
+const hideNavbarRoutes = ["/login", "/register", "/"];
+
+const showNavbar = !hideNavbarRoutes.includes(location.pathname);
+
+
+
   return (
-    <Router> 
-      <Navbar  />
+    <> 
+    {showNavbar && <Navbar />}  {/* Conditionally render Navbar */}
       <Routes>
         <Route path="/" element={<StarterPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />  {/* Render AppContent, which controls the Navbar and Routes */}
     </Router>
   );
 }
+
 
 export default App;
