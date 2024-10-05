9743838cd9db07478e94305efca6896cf2b6f0f7
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2024-10-05 12:17:42 -0400

    create base chart template to accept stock data
    import stock data hook and pass into chart component

--- frontend/profitpulsex/src/component/Chart.jsx
+++ frontend/profitpulsex/src/component/Chart.jsx
@@ -1,43 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
+import useStockData from "../hooks/useStockData";
 
-const Chart = () => {
-  const [stockData, setStockData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchStockData = async () => {
-      const apiKey = "Yv28NTcIJ079vty4ZCfwGQEy1ytr1T8_0";
-      const symbol = "TSLA";
-
-      try {
-        const response = await axios.get(
-          `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`
-        );
-
-        const data = response.data["Time Series (Daily)"];
-        const stockDataFormatted = Object.keys(data)
-          .slice(0, 7)
-          .map((date) => ({
-            date,
-            close: data[date]["4. close"],
-          }));
-
-        setStockData(stockDataFormatted);
-        setLoading(false);
-      } catch (error) {
-        setError("Failed to fetch stock data");
-        setLoading(false);
-      }
-    };
-
-    fetchStockData();
-  }, []);
+const Chart = ({ symbol }) => {
+  const { stockData, loading, error } = useStockData(symbol);
 
   return (
     <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">Last 7 Days Stock Prices</h2>
+      <h2 className="text-2xl font-bold mb-4">{symbol} Stock Prices</h2>
       {loading && <p>Loading stock data...</p>}
       {error && <p className="text-red-500">{error}</p>}
       {!loading && !error && stockData.length > 0 && (
