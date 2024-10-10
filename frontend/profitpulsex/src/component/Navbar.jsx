138d9fb10210fad63c9febfddfa034e07368cd28
Tareksilmi20 <tarek.silmi24@gmail.com>
2024-10-09 22:52:58 -0400

    Update Navbar.jsx

--- frontend/profitpulsex/src/component/Navbar.jsx
+++ frontend/profitpulsex/src/component/Navbar.jsx
@@ -1,16 +1,15 @@
-// src/components/Navbar.jsx
-
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function Navbar() {
     console.log("Navbar rendered");
   return (
     <nav className="bg-blue-500 p-4 fixed top-0 left-0 right-0 z-50">
       <ul className="flex justify-between text-white">
-        <li><a href="/">Home</a></li>
-        <li><a href="/dashboard">Dashboard</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/register">Register</Link></li>
       </ul>
     </nav>
   );
